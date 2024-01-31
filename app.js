36e90989f38b243288b4194526462799ff898513
ila67rus <ila67rus@gmail.com>
2024-01-31 21:35:42 +0300

    Добавил команды

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
-console.log('MyRepository') 
-console.log('MyRepository') 
+console.log('MyRepository')
+
+//PS F:\p311chernov\papka> git config --global user.email "you@example.com"
+//  git config --global user.email "ila67rus@gmail.com"
+// git config --global user.name "ila67rus"
+//  git commit -m "Init repositories"
+// PS F:\p311chernov\papka> git push --set-upstream origin main
+//
+// PS F:\p311chernov\papka> git add app.js
+// PS F:\p311chernov\papka> git status
+//PS F:\p311chernov\papka> git commit -m "First Change"
+//PS F:\p311chernov\papka> git push
+
+//новая ветка
+//git checkout -b(новая ветка) my_second_branch
+//все ветки
+//git branch (* текущая ветка)
+//вернуться в другую ветку
+//git checkout main
